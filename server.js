1f7f6625c12feb3035c1a3045e338ec5ead1b307
huy <emptydn1@gmail.com>
2023-07-19 09:59:31 +0700

    base-project

---
+++ server.js
@@ -0,0 +1,10 @@
+import http from 'http';
+
+import app from './app/app.js';
+
+
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+server.listen(PORT, console.log(`server is running on port ${PORT}`));
+

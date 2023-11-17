a8a7beeb4ddb67de8d31bc2a4c5e565f9628f3b6
huy <nguyenduyhuydn1@gmail.com>
2023-11-17 21:34:45 +0700

    o

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 import http from 'http';
 
 import app from './app/app.js';
+// MONGO_URL=mongodb+srv://nguyenduyhuydn1:hFytCtmcAlK75WWX@cluster0.yjqvz6i.mongodb.net/?retryWrites=true&w=majority
 
+// CLOUDINARY_NAME=drbkeffpu
+// CLOUDINARY_API_KEY=517213353523123
+// CLOUDINARY_API_SECRET=K5ffk2AA_H5gkRjl-OWJb9NBfjQ
+// CLOUDINARY_URL=cloudinary://517213353523123:K5ffk2AA_H5gkRjl-OWJb9NBfjQ@drbkeffpu
+
+// STRIPE_KEY=
+// JWT_KEY=keyword1
 
 const PORT = process.env.PORT || 5000;
 
